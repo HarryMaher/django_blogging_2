46ad4516263c6d62b648093d2b5a87606fab9ce0
HarryMaher <harryb.maher@gmail.com>
2018-08-12 13:18:56 -0700

    9/10 complete

---
+++ myblog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from myblog.models import Post
+from myblog.models import Category
+
+
+admin.site.register(Category)
+
+# Category selector inside of posts
+class CategoriesInLine(admin.TabularInline):
+    model = Category.posts.through
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoriesInLine,
+    ]
+# exclude post selector from admin catogires
+# https://docs.djangoproject.com/en/2.1/ref/contrib/admin/
+
+
+# class CategoryAdmin(admin.ModelAdmin):
+#   inlines = [
+#   CategoriesInLine
+#   ]
+
+# admin.site.register(Post)
